115baa7e640ce717cf1c84e49fc5f968dda1006e
Kristen Jacob <jacob.kristen.c@gmail.com>
2023-01-23 20:25:34 -0800

    S2 Sun and Moon

---
+++ src/SunAndMoon/SunAndMoon.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+import sunSrc from './images/sun.png';
+import moonSrc from './images/moon.png';
+
+const SunAndMoon = () => {
+
+
+    //JavaScript
+
+    const [imgSrc, imgSrcUpdate] = useState(sunSrc);
+
+    const changeToMoon = () => {
+        // console.log("Moon");
+        imgSrcUpdate(moonSrc);
+    }
+
+    const changeToSun = () => {
+        // console.log("Sun");
+        imgSrcUpdate(sunSrc);
+    }
+
+    // HTML
+    return (
+        <SunAndMoonStyled>
+            <h2>Sun And Moon</h2>
+            <img src={imgSrc} onMouseEnter={changeToMoon} onMouseLeave={changeToSun}/>
+            </SunAndMoonStyled>
+        );
+};
+
+export default SunAndMoon;
+
+// CSS: Styles
+const SunAndMoonStyled = styled.div`
+    text-align: center;
+
+    h2 {
+        font-size: 50px;
+        color: teal;
+    }
+
+    img {
+        border: solid 20px teal;
+        margin: 30px;
+        padding: 20px;
+        background-color: #073e3e;
+    }
+`;
