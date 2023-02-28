cde896d29e4f18af2054554cde9417028835e8c3
Kristen Jacob <jacob.kristen.c@gmail.com>
2023-02-27 20:45:31 -0800

    S6: Completed Staff Members

--- src/Pages/Primary/Staff/Staff.jsx
+++ src/Pages/Primary/Staff/Staff.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
 import styled from 'styled-components';
+
+/* Components ---------------------------*/
+import StaffList from './StaffList';
 
 const Staff = () => {
     return (
         <StaffStyled className='Staff'>
             <h1>Staff</h1>
+            <StaffList />
         </StaffStyled>
     );
 }
